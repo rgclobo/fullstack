3b2f4df28ccad1df7a99046b5fcdc8e19991a8d7
rgclobo <rick.gimenez@hotmail.com>
2025-09-16 14:20:40 -0300

    JS - Learning variables

---
+++ Javascript/JS_basic/01_variables/scripts.js
@@ -0,0 +1,50 @@
+// Declara uma variável sem valor.
+var user
+console.log(user)
+
+// Declara uma variável com valor
+var email = "rick.gimenez@hotmail.com"
+console.log(email)
+
+// Substitui o valor da variável
+
+email = "joao@joao.com"
+console.log(email)
+
+// alert(email)
+
+//Case Sensitive
+/* 
+    Quando uma lingugem de programção é Case sensitive
+    significa a letras maiúsculas e minúsculas
+*/
+
+var product = "Teclado Mecânico"
+var Product = "Mouse Gamer"
+console.log(product)
+console.log(Product)
+
+
+// let = Declara uma variável sem valor
+let user1
+console.log(user1)
+
+// Alterando o valor da variável
+user1 = "Ricardo Lobo"
+console.log(user1)
+
+
+// const
+//Uma constante cria um valor cujo o valor é fixo (Não pode ser alterado)
+const number = 42
+console.log(number)
+
+// Ira gerar um erro pq não pode ser alterado
+number = 55
+console.log(number)
+
+//Nome de variaveis - Case sensitive
+
+
+
+//Recomendações
