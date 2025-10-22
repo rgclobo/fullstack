9957d1b7585f6ca8dde9dba58fc8a1d25067c599
rgclobo <rick.gimenez@hotmail.com>
2025-10-22 14:12:35 -0300

    Capturing input event | Accepting only numbers | Formatting the currency

---
+++ Javascript/JS_intermediat/07_Refound_project/scripts.js
@@ -0,0 +1,25 @@
+// Seleciona os elementos do formulário
+const amount = document.getElementById('amount')
+
+// Captura o evento de input para formatar o valor
+amount.oninput = () => {
+    // Obtém o valor atual do input e remove os caracteres não numéricos
+    let value = amount.value.replace(/\D/g, '')
+
+    // Transforma o valor em centavos (Exemplo: 15/ 100 = 1.5 é equivalente a R$1,50)
+    value = Number(value) / 100
+
+    //Atualiza o valor do input
+    amount.value = formatCurrencyBRL(value)
+}
+
+function formatCurrencyBRL(value){
+    // Formata o valor no padrão BRL (Real brasileiro)
+    value = value.toLocaleString('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+    })
+    
+    // Retorna o valor formatado
+    return value
+}
