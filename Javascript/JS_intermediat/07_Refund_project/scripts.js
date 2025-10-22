48eb8c369970a62296cff398aaf174898cb481d5
rgclobo <rick.gimenez@hotmail.com>
2025-10-22 15:23:58 -0300

    Getting form data

--- Javascript/JS_intermediat/07_Refund_project/scripts.js
+++ Javascript/JS_intermediat/07_Refund_project/scripts.js
@@ -1,5 +1,8 @@
 // Seleciona os elementos do formulário
+const form = document.querySelector('form')
 const amount = document.getElementById('amount')
+const expense = document.getElementById('expense')
+const category = document.getElementById('category')
 
 // Captura o evento de input para formatar o valor
 amount.oninput = () => {
@@ -22,4 +25,8 @@ function formatCurrencyBRL(value){
     
     // Retorna o valor formatado
     return value
+}
+
+form.onsubmit = (event) => {
+    event.preventDefault
 }
