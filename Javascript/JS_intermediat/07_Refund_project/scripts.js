ac35735588b7a9b403015536c2bc5ed666e4fdf5
rgclobo <rick.gimenez@hotmail.com>
2025-10-23 16:26:46 -0300

    Remove expense items

--- Javascript/JS_intermediat/07_Refund_project/scripts.js
+++ Javascript/JS_intermediat/07_Refund_project/scripts.js
@@ -154,3 +154,16 @@ function updateTotals() {
         alert('Não foi possível atualizar os totais.')
     }
 }
+
+// Evento que captura o clique nos itens da lista.
+expenseList.addEventListener('click', function(event) {
+    // Verifica se o elemento clicado é o ícone de remover
+    if(event.target.classList.contains('remove-icon')){
+        // Obtém a li pai do elemento clicado
+        const item = event.target.closest('.expense')
+        // Remove o item da lista
+        item.remove()
+    }
+    // Atualiza os totais
+    updateTotals()
+})
