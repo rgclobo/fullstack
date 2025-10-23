a9193db57bce48317e02674a6082818121d7dd5d
rgclobo <rick.gimenez@hotmail.com>
2025-10-23 15:43:25 -0300

    Create function: updates the amount of expenses

--- Javascript/JS_intermediat/07_Refund_project/scripts.js
+++ Javascript/JS_intermediat/07_Refund_project/scripts.js
@@ -6,6 +6,7 @@ const category = document.getElementById('category')
 
 // Seleciona os elementos da lista
 const expenseList = document.querySelector('ul')
+const expensesQuantity = document.querySelector('aside header p span')
 
 // Captura o evento de input para formatar o valor
 amount.oninput = () => {
@@ -92,8 +93,27 @@ function expenseAdd(newExpense){
 
         // Adiciona o item na list.
         expenseList.append(expenseItem)
+
+        // Atualiza os totais
+            updateTotals()
     } catch (error){
         alert('Não foi possivel atualizar a lista de despesas.')
         console.log(error)
+    }
+}
+
+// Atualiza os totais
+function updateTotals() {
+    try{
+        // Recupera todos os itens (li) da lista (ul)
+        const items = expenseList.children
+
+        //Atualiza a quantidade de itens da lista.
+        expensesQuantity.textContent = `${items.length} ${items.length > 1 ? 'despesas' : 'despesa'}`
+
+
+    } catch(error) {
+        console.log(error)
+        alert('Não foi possível atualizar os totais.')
     }
 }
